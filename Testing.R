317de0e0e62886c11da5a1103d62a90d7a3d14cb
Derek Brown <bderek38@gmail.com>
2022-04-06 17:31:07 -0400

    Update Testing.R

--- Testing.R
+++ Testing.R
@@ -1,10 +1,11 @@
 #Dawson
 dawson <- (3.48+4.62)*1.08 + 1; dawson #$1 is for walmart plus. 
 
-#Kyle
-kyle <- (1.78+4.48+9.88+6.24)*1.08 + 1; kyle
+
 
 #Jack
 jack <- (6.24+2.36+5.44+2.98+4.24+1.77)*1.08+1; jack
 
 93.2 - sum(dawson,kyle,jack)
+
+# Hi
